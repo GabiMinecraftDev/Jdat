19d092b45234a5e362c9dd4d9eb5d83d8e2504a2
GabiMinecraftDev <gabriel.m020913@gmail.com>
2026-06-28 21:47:38 +0200

    Update JdatSetup.bat

--- setup/JdatSetup.bat
+++ setup/JdatSetup.bat
@@ -7,12 +7,12 @@ echo Installing Jdat to %DEST_DIR%...
 mkdir "%DEST_DIR%\bin" 2>nul
 mkdir "%DEST_DIR%\include" 2>nul
 
-set "BASE_URL=https://github.com/TON_USERNAME/TON_REPO/releases/latest/download"
+set "BASE_URL=https://github.com/GabiMinecraftDev/Jdat"
 
-curl -L -o "%DEST_DIR%\bin\libjdat.dll" "%BASE_URL%/libjdat.dll"
-curl -L -o "%DEST_DIR%\include\jdat.h" "%BASE_URL%/jdat.h"
+curl -L -o "%DEST_DIR%\libjdat.dll" "%BASE_URL%/bin/libjdat.dll"
+curl -L -o "%DEST_DIR%\jdat.h" "%BASE_URL%/include/jdat.h"
 
 echo Installation complete.
-echo Library location: %DEST_DIR%\bin\libjdat.dll
-echo Header location: %DEST_DIR%\include\jdat.h
+echo Library location: %DEST_DIR%\libjdat.dll
+echo Header location: %DEST_DIR%\jdat.h
 pause
