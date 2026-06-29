be0cf447a32f2ba4fc9d131371bb85207218628c
GabiMinecraftDev <gabriel.m020913@gmail.com>
2026-06-29 09:45:52 +0200

    Update installation paths in JdatSetup.bat

--- setup/JdatSetup.bat
+++ setup/JdatSetup.bat
@@ -1,18 +1,25 @@
 @echo off
 setlocal
 
+mkdir "C:\Program Files\Jdat_Engine"
 set "DEST_DIR=%ProgramFiles%\Jdat_Engine"
 
 echo Installing Jdat to %DEST_DIR%...
-mkdir "%DEST_DIR%\bin" 2>nul
-mkdir "%DEST_DIR%\include" 2>nul
 
-set "BASE_URL=https://github.com/GabiMinecraftDev/Jdat"
+set "BASE_URL=https://github.com/GabiMinecraftDev/blob/main"
 
 curl -L -o "%DEST_DIR%\libjdat.dll" "%BASE_URL%/bin/libjdat.dll"
 curl -L -o "%DEST_DIR%\jdat.h" "%BASE_URL%/include/jdat.h"
 
+if %errorlevel% neq 0 (
+    echo Une erreur est survenue (Code: %errorlevel%).
+    echo Le programme va s'arreter.
+    pause
+    exit /b %errorlevel%
+)
+
 echo Installation complete.
-echo Library location: %DEST_DIR%\libjdat.dll
-echo Header location: %DEST_DIR%\jdat.h
+echo Library location: %DEST_DIR%\bin\libjdat.dll
+echo Header location: %DEST_DIR%\include\jdat.h
 pause
+exit /b
